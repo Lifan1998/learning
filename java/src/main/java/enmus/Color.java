0bc0e989be02d10d0049aa67a5f6e24366deddac
lifan <2224779926@qq.com>
2020-07-14 18:17:03 +0800

    test

---
+++ java/src/main/java/enmus/Color.java
@@ -0,0 +1,12 @@
+package enmus;
+
+/**
+ * @author fan.li
+ * @date 2020-06-28
+ * @description
+ */
+public enum Color
+{
+    RED, GREEN, BLUE;
+}
+
---
+++ java/src/main/java/enmus/Test.java
@@ -0,0 +1,12 @@
+package enmus;
+
+/**
+ * @author fan.li
+ * @date 2020-06-28
+ * @description
+ */
+
+public class Test {
+
+}
+
